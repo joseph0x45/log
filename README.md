7807a69db26a8609f4275658a224088d5983936c
joseph0x45 <zozozozeph@gmail.com>
2025-06-09 16:30:46 +0000

    show entries for today or specific date

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # log
-Log anything anytime from anywhere
+Log anything anytime from anywhere. This is a very simple tool I made to be able to log
+stuff anytime I want. I originally had the idea of something like this when I started my weight
+loss journey.
+
+## Interface
+- Add a new log entry
+`log <title> <content>`
+
+Features:
+- [x] Add log entry
+- [x] Show content of current day's log file
+- [x] Show content of a specific day's log file
+- [ ] List all logs
+- [ ] Clean log older than X days
+- [ ] Human readable filters (log show yesterday/today-2...)
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <linux/limits.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,18 +9,29 @@
 #define LOG_HOME_DIR ".log_home"
 #define DATE_SIZE 11
 #define TIME_SIZE 9
+#define MAX_LINE_SIZE 1024
 
 void print_usage(void);
 
 char *get_log_home(void);
 
-int log_entry(char *title, char *content);
-
 void get_current_date_time(char *date_out, size_t date_size, char *time_out,
                            size_t time_size);
 
+void get_log_file_path(char *path_out, size_t path_size, char *current_date);
+
+int log_entry(char *title, char *content);
+
+int show_entries(char *date);
+
 int main(int argc, char **argv) {
+  if (argc == 2 && (strcmp(argv[1], "show") == 0)) {
+    return show_entries(NULL);
+  }
   if (argc == 3) {
+    if (strcmp(argv[1], "show") == 0) {
+      return show_entries(argv[2]);
+    }
     return log_entry(argv[1], argv[2]);
   }
   print_usage();
@@ -58,6 +70,11 @@ void get_current_date_time(char *date_out, size_t date_size, char *time_out,
   }
 }
 
+void get_log_file_path(char *path_out, size_t path_size, char *current_date) {
+  char *log_home = get_log_home();
+  snprintf(path_out, path_size, "%s/%s.log", log_home, current_date);
+}
+
 int log_entry(char *title, char *content) {
   if (strcmp(title, "") == 0 || strcmp(content, "") == 0) {
     print_usage();
@@ -67,16 +84,38 @@ int log_entry(char *title, char *content) {
   get_current_date_time(current_date, sizeof(current_date), current_time,
                         sizeof(current_time));
   char log_file_path[PATH_MAX];
-  char *log_home = get_log_home();
-  snprintf(log_file_path, sizeof(log_file_path), "%s/%s.log", log_home,
-           current_date);
+  get_log_file_path(log_file_path, sizeof(log_file_path), current_date);
   FILE *f = fopen(log_file_path, "a");
   if (!f) {
-    fprintf(stderr, "Error: Could not open log file at %s", log_file_path);
+    fprintf(stderr, "Error: Could not open log file at %s: ", log_file_path);
     perror("");
     return 1;
   }
   fprintf(f, "[%s] %s: %s\n", current_time, title, content);
+  fclose(f);
+  return 0;
+}
+
+int show_entries(char *date) {
+  char log_date[DATE_SIZE];
+  if (!date) {
+    get_current_date_time(log_date, sizeof(log_date), NULL, 0);
+    date = log_date;
+  }
+
+  char log_file_path[PATH_MAX];
+  get_log_file_path(log_file_path, sizeof(log_file_path), date);
+
+  FILE *f = fopen(log_file_path, "r");
+  if (!f) {
+    fprintf(stderr, "Error: Could not open log file at %s: ", log_file_path);
+    perror("");
+    return 1;
+  }
+  char line[MAX_LINE_SIZE];
+  while (fgets(line, sizeof(line), f)) {
+    fputs(line, stdout);
+  }
   fclose(f);
   return 0;
 }
